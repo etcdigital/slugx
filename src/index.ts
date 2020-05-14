ab8b06f8fd0c17dffba5ad09588eb51ad2752870
Tiago Souza <tiago@explorethecuriosity.com>
2020-05-14 10:08:40 -0300

    feat: :boom: Added more options for validate, improve options to create slugs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
 import charMap from './charmap';
 
-type Options = {
+type CreateOptions = {
   separator?: string;
-  decamelize?: boolean;
   lowercase?: boolean;
   strict?: boolean;
 };
 
-export const create = (value: string, options: Options = {}): string => {
+export const create = (value: string, options: CreateOptions = {}): string => {
+  const opts: CreateOptions = { separator: '-', lowercase: true, strict: false, ...options };
   if (typeof value !== 'string') {
     throw new Error('slugx.create: string argument expected');
   }
-
-  const separator = options.decamelize ? '' : options.separator || '-';
 
   let slug = value
     .split('')
@@ -24,33 +22,43 @@ export const create = (value: string, options: Options = {}): string => {
     .trim()
     // convert spaces to replacement character
     // also remove duplicates of the replacement character
-    .replace(new RegExp('[\\s' + separator + ']+', 'g'), separator);
+    .replace(new RegExp('[\\s' + opts.separator + ']+', 'g'), opts.separator);
 
-  if (options.lowercase !== false) {
+  if (opts.lowercase !== false) {
     slug = slug.toLowerCase();
   }
 
-  const isStrict = options.strict === false ? false : true;
+  const isStrict = opts.strict === false ? false : true;
   if (isStrict) {
     // remove anything besides letters, numbers, and the replacement char
-    slug = slug.replace(new RegExp('[^a-zA-Z0-9' + separator + ']', 'g'), '');
+    slug = slug.replace(new RegExp('[^a-zA-Z0-9' + opts.separator + ']', 'g'), '');
   }
 
   return slug;
 };
 
-export const validate = (slug: string, minChar = 3, maxChar = 32): boolean => {
-  const genSlug = create(slug, { lowercase: true });
+type ValidateOptions = {
+  min?: number;
+  max?: number;
+  allowOnlyNumbers?: boolean;
+  slugOptions?: CreateOptions;
+};
+
+export const validate = (slug: string, options: ValidateOptions = {}): boolean => {
+  const opts: ValidateOptions = { min: 1, max: 1000, allowOnlyNumbers: true, ...options };
+  const genSlug = create(slug, { lowercase: true, ...(options.slugOptions || {}) });
   if (genSlug !== slug) {
     return false;
   }
-  if (minChar) {
-    if (genSlug.length < minChar) {
-      return false;
-    }
+  if (genSlug.length < opts.min) {
+    return false;
   }
-  if (maxChar) {
-    if (genSlug.length > maxChar) {
+  if (genSlug.length > opts.max) {
+    return false;
+  }
+  if (!opts.allowOnlyNumbers) {
+    // If has parseInt number, has only numbers
+    if (parseInt(slug, 10)) {
       return false;
     }
   }
